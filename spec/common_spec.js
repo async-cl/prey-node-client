a86c87387a890277d9823eafe05856a432adf712
Ritchie Turner <ritchie@async.cl>
2012-12-04 12:08:14 -0300

    implement common_spec

--- spec/common_spec.js
+++ spec/common_spec.js
@@ -1,23 +1,34 @@
-var common = require('./../lib/common');
+var common = require('./../lib/common'),
+	  system = require('../lib/system'),
+		should = require('should'),
+		helpers = require('./helpers'),
+		fs = require('fs'),
+		join = require('path').join;
 
 describe('common', function(){
 
 	it('should set Preys ROOT_PATH in process.env', function(){
-		true.should.equal(true);
+		should.exist(process.env.ROOT_PATH);
 	});
 
 	describe('config path', function(){
 
 		it('should default to OS config path', function(){
-
+			common.config_path.should.equal(system.paths.config)
 		})
 
-		it('should check if path is writable', function(){
-
+		it('directory exists',function() {
+			var stat = fs.statSync(common.config_path)
+			should.exist(stat)
+			stat.isDirectory().should.equal(true)		
 		})
 
-		it('should check if config file is readable', function(){
+		it('should check if path is writable', function(done){
+			helpers.checkPermission (common.config_path, 2, done);
+		})
 
+		it('should check if config file is readable', function(done){
+			helpers.checkPermission(join(common.config_path, common.config_file),2,done)
 		})
 
 	})
